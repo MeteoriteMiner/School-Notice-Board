771ee670ee7376ec5703dfd215673e6d38a649d4
MeteoriteMiner <anirban.saha.anjan.03@gmail.com>
2025-08-20 18:07:20 +0600

    Add signout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,28 @@ import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem
 import { Link, useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon, FaSun} from 'react-icons/fa';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { signoutSuccess } from '../redux/user/userSlice';
+
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const {currentUser} = useSelector(state => state.user);
+    const handleSignout = async () => {
+          try {
+            const res = await fetch('/api/user/signout', {
+              method: 'POST',
+            });
+            if (!res.ok) {
+              console.log(data.message);
+            } else{
+              dispatch(signoutSuccess());
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm 
@@ -50,7 +67,7 @@ export default function Header() {
                             <DropdownItem>Profile</DropdownItem>
                         </Link>
                         <DropdownDivider />
-                        <DropdownItem>Sign out</DropdownItem>
+                        <DropdownItem onClick={handleSignout}>Sign out</DropdownItem>
                     </DropdownHeader>
 
                 </Dropdown>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -48,7 +48,12 @@ const userSlice = createSlice({
          deleteUserFailure: (state, action) => {
             state.loading = false;
             state.error = action.payload;
-         }
+         },
+         signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+         },
     },
 });
 
@@ -62,6 +67,7 @@ export const {
     deleteUserStart,
     deleteUserSuccess,
     deleteUserFailure,
+    signoutSuccess,
 } = userSlice.actions;
 
 export default userSlice.reducer;
